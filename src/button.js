0eeb47745bd833c52b0d29752168feb5a9dbc039
Colter Ulrich <cju5025@gmail.com>
2021-11-18 19:16:34 -0700

    Adds Click Handler

--- src/button.js
+++ src/button.js
@@ -3,9 +3,14 @@ import { Component } from 'react';
 import './button.css'
 
 export default class Button extends Component {
+
+    handleClick () {
+        console.log(Date.now())
+    }
+
     render () {
         return (
-            <button id="tempo-tap-button"></button>
+            <button id="tempo-tap-button" onClick={this.handleClick}></button>
         )
     }
 }
